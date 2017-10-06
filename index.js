4d33f7861d4b602f546adb1780c1af695b81ce8c
Fabien Villepinte <fabien.villepinte@gmail.com>
2017-10-06 11:51:54 +0200

    Reduce the number of calls to isFunction

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const isFunction = require('is-function')
 
 const doWheil = (d0, wh1le) => {
   if (!isFunction(d0)) { throw new TypeError('d0 is no function!, D0 MUST BE A FUNCTION') }
-  if (!isBoolean(wh1le) && !isFunction(wh1le)) {
+  const wh1leIsFunction = isFunction(wh1le)
+  if (!isBoolean(wh1le) && !wh1leIsFunction) {
     throw new TypeError('wh1le is no boolean or boolyfunc!, WH1LE MUST BE A BOOLEAN OR A BOOLYFUNC')
   }
   do {
     d0()
-  } while(isFunction(wh1le) ? wh1le() : wh1le)
+  } while(wh1leIsFunction ? wh1le() : wh1le)
 }
 
 module.exports = doWheil
